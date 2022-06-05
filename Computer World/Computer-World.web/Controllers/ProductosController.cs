dbecc0111fd62baeee1ffaab0a758467593c3651
WILBERT ENRRIQUE CASTILLO NOLASCO <wilbert.castillo@unah.hn>
2022-06-04 20:21:34 -0600

    navegacion

---
+++ Computer World/Computer-World.web/Controllers/ProductosController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Computer_World.web.Models;
+
+namespace Computer_World.web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto = new ProductoModel();
+            producto.Id = 1;
+            producto.Descripcion = "computadora";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "computadora2";
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "computadora3";
+
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+            return View(listadeProductos);
+        }
+    }
+}
