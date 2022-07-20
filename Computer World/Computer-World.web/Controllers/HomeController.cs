bdfeb160d0b59104017b6249f06f9a7f34513959
WILBERT ENRRIQUE CASTILLO NOLASCO <wilbert.castillo@unah.hn>
2022-07-20 00:58:27 -0600

    agregando la plantilla para nuestro sitio web publico.

--- Computer World/Computer-World.web/Controllers/HomeController.cs
+++ Computer World/Computer-World.web/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Computer_World.BL;
 using System.Web.Mvc;
+using System.Configuration;
 
 namespace Computer_World.web.Controllers
 {
@@ -11,7 +9,11 @@ namespace Computer_World.web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductosActivos();
+            ViewBag.adminWebsiteUrl= ConfigurationManager.AppSettings["adminWebsiteUrl"];
+            return View(listadeProductos);
+            
         }
     }
 }
